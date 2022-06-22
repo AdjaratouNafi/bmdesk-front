113d7bf40e944d73b2a6aaeb3f2651b5cf9d9862
AdjaratouNafi <77303379+AdjaratouNafi@users.noreply.github.com>
2022-06-22 17:11:25 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    Base_API_URL: 'https://localhost:44322',
-    Site_URL: 'http://localhost:4200',
+    Base_API_URL: 'https://bmdesk-api1-bmdesk.apps.cluster.heritage.africa ',
+    Site_URL: 'https://bmdesk-api1-bmdesk.apps.cluster.heritage.africa ',
     Expiration_Time: 60
 };
 
